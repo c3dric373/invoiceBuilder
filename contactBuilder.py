b4eb665c17ae0d8db440a1c9bb2033103c7785be
cmc373 <milinaire.c@gmail.com>
2018-03-07 18:29:09 +0100

    new file:   contactBuilder.py         building new contact in correct repo

---
+++ contactBuilder.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+from fileHelper import *
+
+firma = input("Firma: \n")
+name =  input("Name: \n")
+street =  input("Street: \n")
+zip_c =  input("ZIP: \n")
+city =  input("City: \n")
+service = input("Service: \n")
+fee = input("Fee: \n")
+training_hours = input("Training hours: \n")
+
+new_data = [firma, name, street, zip_c, city]
+template_data = ["#firma","#name","#street","#zip","#city"]
+new_invoice_data = [service,fee,training_hours]
+template_invoice_data = ["#service","#fee","#training_hours"]
+
+# Building the new data  file
+invoice_template_directory = '/home/c3dric/Perso/Klein Unternehmen/latex-rechnung/' + firma + name
+os.makedirs(invoice_template_directory)
+os.chdir(invoice_template_directory)
+subprocess.run(['cp','-a','/home/c3dric/Perso/Klein Unternehmen/latex-rechnung/original/.', invoice_template_directory])
+openAndReplace('data.tex','data.tex',template_data,new_data)
+
+# Building the new invoice file
+openAndReplace('invoice.tex','invoice.tex',template_invoice_data, new_invoice_data)
